beae3e7fc8266176b10ce9a933f32bb00f98d2c2
Josh <josh.sk.kim@gmail.com>
2020-10-17 16:12:20 -0700

    Added links

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+
+
+/**
+ * Constants for links
+ */
+
+ const links = [
+  { 'name': 'Cloudflare', 'url': 'https://www.cloudflare.com/' },
+  { 'name': 'DoorDash', 'url': 'https://www.doordash.com/' },
+  { 'name': 'Shopify', 'url': 'https://www.shopify.com/' },
+ ];
+
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
 /**
  * Respond with hello worker text
  * @param {Request} request
